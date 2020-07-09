499f69c589b85ad8a821fa8be9f80f1b7e678225
muhsina.akter <muhsina.akter2@gmail.com>
2020-07-10 00:29:24 +0600

    Form Validation Done

--- index.php
+++ index.php
@@ -10,13 +10,50 @@
 </head>
 <body>
 	
-	
+<?php
+/**
+ * Student form data manage
+ */
+
+if( isset($_POST['submit'])){
+
+    //Value get
+     $name = $_POST['name'];
+     $email = $_POST['email'];
+     $cell = $_POST['cell'];
+
+    //File Manage
+    $img = $_FILES['photo'];
+
+    if( empty($name) || empty($email) || empty($cell) ){
+        $mess = "<p class=\"alert alert-danger \">All fields are required ! <button class=\"close \" data-dismiss=\"alert \">&times;</button> </p>";
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $mess = "<p class=\"alert alert-danger \">Invalid Email Address ! <button class=\"close \" data-dismiss=\"alert \">&times;</button> </p>";
+    }
+
+    else{
+
+    }
+
+}
+
+?>
+
 
 	<div class="wrap">
         <a class="btn btn-primary btn-sm" href="data.php">All Students</a>
 		<div class="card shadow">
 			<div class="card-body">
 				<h2>Sign Up</h2>
+
+                <?php
+                 if( isset($mess)) {
+                     echo $mess;
+                 }
+
+                ?>
+
 				<form action="<?php echo $_SERVER['PHP_SELF']  ?>" method="post" enctype="multipart/form-data">
 					<div class="form-group">
 						<label for="">Name</label>
